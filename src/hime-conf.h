6bf7d8e966a362636278f689403a0d21cea1c3fd
Petercommand <petercommand@gmail.com>
2015-08-23 11:38:45 +0800

    fix merge

--- src/hime-conf.h
+++ src/hime-conf.h
@@ -140,7 +140,7 @@ extern char *phonetic_speak_sel;
 extern int tsin_space_opt, tsin_tone_char_input;
 
 
-extern char *tsin_phrase_line_color, *hime_cursor_color, *hime_font_name, *hime_sel_key_color;
+extern char *tsin_phrase_line_color, *hime_cursor_color, *hime_font_name, *hime_sel_key_color, *hime_icon_dir;
 extern unich_t eng_full_str[], eng_half_str[], cht_full_str[];
 extern char *eng_color_full_str, *eng_color_half_str, *cht_color_full_str;
 extern char *hime_win_color_fg, *hime_win_color_bg;
